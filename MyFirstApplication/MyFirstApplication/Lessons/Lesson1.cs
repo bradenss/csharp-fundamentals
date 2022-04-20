2ba6a62297a140e58244b4c1b96be09190e11f42
bschnieders <bschnieders@WSUtech.edu>
2022-04-20 04:23:02 -0500

    CAC-103 Lesson1 Value Types

---
+++ MyFirstApplication/MyFirstApplication/Lessons/Lesson1.cs
@@ -0,0 +1,147 @@
+﻿
+namespace MyFirstApplication;
+
+/*
+ This classgoes over Value Types.
+ */
+internal class Lesson1
+{
+    // Intergral Signed
+
+    // sbyte range -128 to 127
+    sbyte mySByte = 2;
+    public void SByteMinMax()
+    {
+        Console.WriteLine(SByte.MinValue);
+        Console.WriteLine(SByte.MaxValue);
+    }
+
+    // short range -32,768 to 32,767
+    short myShort = 6;
+
+    public void ShortMinMax()
+    {
+        Console.WriteLine(Int16.MinValue);
+        Console.WriteLine(Int16.MaxValue);
+    }
+
+    // int range -2,147,483,648 to 2,147,483,647
+    int myInt = 25685;
+    public void IntMinMax()
+    {
+        Console.WriteLine(Int32.MinValue);
+        Console.WriteLine(Int32.MaxValue);
+    }
+
+    // long range -9,223,372,36,854,775,808 to 
+    // 9,223,372,036,854,775,807
+    long myLong = 123456789L;
+
+    public void LongMinMax()
+    {
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+    }
+    // Intergral Unsigned
+
+    // byte range 0 - 255
+    byte myByte = 4;
+
+    public void ByteMnMax()
+    {
+        Console.WriteLine(Byte.MinValue);
+        Console.WriteLine(Byte.MaxValue);
+    }
+
+    // ushort range 0 - 65,535
+    ushort myUShort = 42;
+
+    public void UshortMinMax()
+    {
+        Console.WriteLine(UInt16.MinValue);
+        Console.WriteLine(UInt16.MaxValue);
+    }
+
+    // unint range 0 -4,294,967,295
+    uint myUint = 4532U;
+
+    public void UIntMinMAx()
+    {
+        Console.WriteLine(UInt32.MinValue);
+        Console.WriteLine(UInt32.MaxValue);
+    }
+
+    // ulong range 0 - 18,446,744,076,709,551,615
+    ulong myULong = 948372UL;
+
+    public void ULongMinMax()
+    {
+        Console.WriteLine(UInt64.MinValue);
+        Console.WriteLine(UInt64.MaxValue);
+    }
+
+    // Flaoting Point / Reeal Numbers
+
+    // float range 3.4E +/- 38
+    float myFloat = -123.6589F;
+
+    public void FloatMinMax()
+    {
+        Console.WriteLine(Single.MinValue);
+        Console.WriteLine(Single.MaxValue);
+    }
+
+    // double range 1.7E +/- 308
+    double myDouble = 12.34D;
+
+    public void DoubleMinMax()
+    {
+        Console.WriteLine(Double.MinValue);
+        Console.WriteLine(Double.MaxValue);
+    }
+
+    // decimal range -7.9228E+24 to 7.9228E+24
+    decimal myDecimal = 13.23M;
+
+    public void DecimalMinMax()
+    {
+        Console.WriteLine(Decimal.MinValue);
+        Console.WriteLine(Decimal.MinValue);
+    }
+
+    //Intergral Literals
+    public void IntergralExample()
+    {
+        // value of 26
+        int value1 = 26; // decimal
+        int value2 = 0x1a; // hexadecimal
+        int value3 = 0b11010; // binary
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+        Console.WriteLine(value3);
+    }
+
+    // Scientific Notation
+    double alpha = 6.26E-22;
+
+    // Underscores
+    long beta = 1_834_432L;
+
+    // Explicit casting is where fractions are tuncated and not rounded
+    // You have to provide permission to cast from a higher type
+    // to a lower type.
+
+    public void ConvertFloastToInt()
+    {
+        int converted = (int)myFloat;
+        Console.WriteLine(converted);
+    }
+
+    // Implicit cast requires not direct permission
+
+    public void LongFromInt()
+    {
+        long someValue = myInt;
+        Console.WriteLine(someValue);
+    }
+}
--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -1,14 +1,13 @@
 ﻿using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // House myHouse = new House();
 // myHouse.DoorOpenClose();
 
 // The below statement calls a local function
 // single line comment
-HouseExample();
 
 /*
  multi-line comment
@@ -18,6 +17,9 @@ They are specific to that memer and can only be called
 inside that member.
  */
 
+// Lesson Examples
+// HouseExample();
+
 void HouseExample()
 {
     House myHouse; new House();
@@ -26,7 +28,7 @@ void HouseExample()
 
 // This local function is for my first exercise lab.
 
-CSharpFunction();
+// CSharpFunction();
 
 void CSharpFunction()
 {
@@ -34,4 +36,14 @@ void CSharpFunction()
     Console.WriteLine("I am new to Csharp.");
     Console.WriteLine("This CSharp course is cool.");
     Console.WriteLine("I am learning stuff every day.");
+}
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntergralExample();
+    myLesson.ConvertFloastToInt();
+    myLesson.LongFromInt();
 }
